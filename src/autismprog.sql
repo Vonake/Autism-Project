b186118e6e223436b9344db901972d2dc4610cdd
Your Name <you@example.com>
2020-10-31 14:18:02 +0300

    added DB to src

---
+++ src/autismprog.sql
@@ -0,0 +1,357 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2020 at 12:15 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `autismprog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_log`
+--
+
+CREATE TABLE `admin_log` (
+  `id` int(20) NOT NULL,
+  `supervisor_id` int(20) NOT NULL,
+  `action` varchar(20) NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `month` varchar(20) NOT NULL,
+  `year` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guardian_details`
+--
+
+CREATE TABLE `guardian_details` (
+  `id` int(11) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `relation_to_student` varchar(40) NOT NULL,
+  `names` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `Student_ID` int(20) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `Age` int(10) NOT NULL,
+  `UserName` varchar(40) NOT NULL,
+  `Password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`Student_ID`, `First_Name`, `Last_Name`, `Age`, `UserName`, `Password`) VALUES
+(3, 'Kelvin', 'Kamau', 2, 'kamaa', 'kijabe'),
+(5, 'Musa', 'Musa', 123, 'test1', 'wqwert'),
+(7, 'Maureen', 'Nthenya', 23, 'maureen', 'maureen');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_location`
+--
+
+CREATE TABLE `student_location` (
+  `id` int(20) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `county` varchar(40) NOT NULL,
+  `subcounty` varchar(40) NOT NULL,
+  `ward` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_log`
+--
+
+CREATE TABLE `student_log` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `action` varchar(400) NOT NULL,
+  `time` varchar(25) NOT NULL,
+  `date` varchar(25) NOT NULL,
+  `month` varchar(2) NOT NULL,
+  `year` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_parent`
+--
+
+CREATE TABLE `student_parent` (
+  `id` int(20) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `parent_status` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_task`
+--
+
+CREATE TABLE `student_task` (
+  `id` int(20) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `task_id` int(20) NOT NULL,
+  `time_taken` varchar(20) NOT NULL,
+  `time` varchar(11) NOT NULL,
+  `date` varchar(11) NOT NULL,
+  `month` varchar(11) NOT NULL,
+  `year` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `Supervisor_ID` int(20) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `Student_Relationship` varchar(40) NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `UserName` varchar(40) NOT NULL,
+  `Password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+INSERT INTO `supervisor` (`Supervisor_ID`, `First_Name`, `Last_Name`, `Student_Relationship`, `Email`, `UserName`, `Password`) VALUES
+(1, 'First super name', 'Last super name', 'Relation', 'email@rmail.com', 'user', 'password'),
+(2, '', '', '', '', '', ''),
+(3, '', '', '', '', '', ''),
+(4, 'first_name ', 'last_name', 'student_rship', 'email', 'username', 'password'),
+(5, 'Kelvin', 'Kamau', 'Father', 'knkamau@garlado.com', 'vonake', 'Vonake'),
+(6, 'Mercy', 'Wambui', 'Mother', 'mums@aut.co.ke', 'Wambo', 'Wambo'),
+(7, 'vonake', 'kamau', 'father', 'knkamau@garlado.co.ke', 'vonake', 'vonake');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `Task_ID` int(20) NOT NULL,
+  `Task_Name` varchar(40) NOT NULL,
+  `Description` varchar(100) NOT NULL,
+  `Graphic` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`Task_ID`, `Task_Name`, `Description`, `Graphic`) VALUES
+(2, 'Handwashing', 'we are testing the insert method', 'No Graphic config yet test'),
+(3, 'Hand Washing', 'how to wash your hands', 'image erer'),
+(4, 'Brush your teath', 'Brush your teath', 'graphic');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_log`
+--
+ALTER TABLE `admin_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supervisor_id` (`supervisor_id`);
+
+--
+-- Indexes for table `guardian_details`
+--
+ALTER TABLE `guardian_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`Student_ID`);
+
+--
+-- Indexes for table `student_location`
+--
+ALTER TABLE `student_location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `student_log`
+--
+ALTER TABLE `student_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `student_parent`
+--
+ALTER TABLE `student_parent`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `student_task`
+--
+ALTER TABLE `student_task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`Supervisor_ID`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`Task_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_log`
+--
+ALTER TABLE `admin_log`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `guardian_details`
+--
+ALTER TABLE `guardian_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `Student_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `student_location`
+--
+ALTER TABLE `student_location`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_log`
+--
+ALTER TABLE `student_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_parent`
+--
+ALTER TABLE `student_parent`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_task`
+--
+ALTER TABLE `student_task`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  MODIFY `Supervisor_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `Task_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin_log`
+--
+ALTER TABLE `admin_log`
+  ADD CONSTRAINT `admin_log_ibfk_1` FOREIGN KEY (`supervisor_id`) REFERENCES `supervisor` (`Supervisor_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `guardian_details`
+--
+ALTER TABLE `guardian_details`
+  ADD CONSTRAINT `guardian_details_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`Student_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student_location`
+--
+ALTER TABLE `student_location`
+  ADD CONSTRAINT `student_location_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`Student_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student_log`
+--
+ALTER TABLE `student_log`
+  ADD CONSTRAINT `student_log_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`Student_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student_parent`
+--
+ALTER TABLE `student_parent`
+  ADD CONSTRAINT `student_parent_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`Student_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student_task`
+--
+ALTER TABLE `student_task`
+  ADD CONSTRAINT `student_task_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`Student_ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_task_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`Task_ID`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
